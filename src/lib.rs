366aef1ba53e42af2120e566b6ca7c7a0d1ddb62
Nicolas Patry <patry.nicolas@protonmail.com>
2022-12-16 13:15:00 +0100

    Rename lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use reqwest::header::{CONTENT_LENGTH, RANGE};
 use std::io::SeekFrom;
@@ -12,8 +13,7 @@ use tokio::sync::Semaphore;
 fn download(url: String, filename: String, max_files: usize, chunk_size: usize) -> PyResult<()> {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .build()
-        .unwrap()
+        .build()?
         .block_on(async { download_async(url, filename, max_files, chunk_size).await })
 }
 
@@ -23,16 +23,22 @@ async fn download_async(
     max_files: usize,
     chunk_size: usize,
 ) -> PyResult<()> {
-    let start = std::time::Instant::now();
+    // let start = std::time::Instant::now();
     let client = reqwest::Client::new();
-    let response = client.head(&url).send().await.unwrap();
+    let response = client
+        .head(&url)
+        .send()
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
     let length = response
         .headers()
         .get(CONTENT_LENGTH)
-        .unwrap()
+        .ok_or(PyException::new_err("No content length"))?
         .to_str()
-        .unwrap();
-    let length: usize = length.parse().unwrap();
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
+    let length: usize = length
+        .parse()
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
 
     let mut handles = vec![];
     let semaphore = Arc::new(Semaphore::new(max_files));
@@ -44,38 +50,71 @@ async fn download_async(
         let client = client.clone();
 
         let stop = std::cmp::min(start + chunk_size - 1, length);
-        let range = format!("bytes={start}-{stop}");
-        let permit = semaphore.clone().acquire_owned().await.unwrap();
+        let permit =
+            semaphore.clone().acquire_owned().await.map_err(|err| {
+                PyException::new_err(format!("Error while downloading: {:?}", err))
+            })?;
         handles.push(tokio::spawn(async move {
-            // Process each socket concurrently.
-            let mut file = tokio::fs::OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(filename)
-                .await
-                .unwrap();
-            file.seek(SeekFrom::Start(start as u64)).await.unwrap();
-            let response = client.get(url).header(RANGE, range).send().await.unwrap();
-            let content = response.bytes().await.unwrap();
-            file.write_all(&content).await.unwrap();
+            let chunk = download_chunk(client, url, filename, start, stop).await;
             drop(permit);
+            chunk
         }));
     }
-    futures::future::join_all(handles).await;
-    let size = length as f64 / 1024.0 / 1024.0;
-    let speed = size / start.elapsed().as_secs_f64();
-    println!(
-        "Took {:?} for {:.2}Mo ({:.2} Mo/s)",
-        start.elapsed(),
-        size,
-        speed
-    );
+
+    // Output the chained result
+    let results: Vec<Result<PyResult<()>, tokio::task::JoinError>> =
+        futures::future::join_all(handles).await;
+    let results: PyResult<()> = results.into_iter().flatten().collect();
+    let _ = results?;
+
+    // let size = length as f64 / 1024.0 / 1024.0;
+    // let speed = size / start.elapsed().as_secs_f64();
+    // println!(
+    //     "Took {:?} for {:.2}Mo ({:.2} Mo/s)",
+    //     start.elapsed(),
+    //     size,
+    //     speed
+    // );
+    Ok(())
+}
+
+async fn download_chunk(
+    client: reqwest::Client,
+    url: String,
+    filename: String,
+    start: usize,
+    stop: usize,
+) -> PyResult<()> {
+    // Process each socket concurrently.
+    let range = format!("bytes={start}-{stop}");
+    let mut file = tokio::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(filename)
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
+    file.seek(SeekFrom::Start(start as u64))
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
+    let response = client
+        .get(url)
+        .header(RANGE, range)
+        .send()
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
+    let content = response
+        .bytes()
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
+    file.write_all(&content)
+        .await
+        .map_err(|err| PyException::new_err(format!("Error while downloading: {:?}", err)))?;
     Ok(())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn rust_dl_py(_py: Python, m: &PyModule) -> PyResult<()> {
+fn hf_transfer(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(download, m)?)?;
     Ok(())
 }
